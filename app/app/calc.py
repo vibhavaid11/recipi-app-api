2d4a8b2127d72efe8bdef67d155056f60e97afd5
vibhavaid <vibha.vaid@keelvar.com>
2023-01-13 13:51:37 +0000

    new files added

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,7 +1,13 @@
 """
-calculator functions
+Calculator functions
 """
-def add(x , y):
+
+
+def add(x, y):
+    """Add x and y and return result."""
     return x + y
-def subtract(x , y):
-    return x - y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,18 +1,22 @@
 """
-
-sample tests
-
+Sample tests
 """
-
 from django.test import SimpleTestCase
+
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-    """test the calc module."""
+    """Test the calc module."""
 
     def test_add_numbers(self):
-        res=calc.add(5 , 6)
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+
         self.assertEqual(res, 11)
+
     def test_subtract_numbers(self):
-        res=calc.subtract(10 , 15)
-        self.assertEqual(res , -5)
+        """Test subtracting numbers."""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
